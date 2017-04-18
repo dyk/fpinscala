87c8a602464b6ab599d0962fe8b859e3d406c091
Przemek Dyk <przemek.dyk@gmail.com>
2017-04-18 15:56:02 +0200

    4.1

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,18 +4,30 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B]
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B]
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B]
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A]
 }
-case class Some[+A](get: A) extends Option[A]
-case object None extends Option[Nothing]
+case class Some[+A](get: A) extends Option[A] {
+  override def map[B](f: (A) => B): Option[B] = Some(f(get))
+  override def getOrElse[B >: A](default: => B): B = get
+  override def flatMap[B](f: (A) => Option[B]): Option[B] = f(get)
+  override def orElse[B >: A](ob: => Option[B]): Option[B] = this
+  override def filter(f: (A) => Boolean): Option[A] = if (f(get)) this else None
+}
+case object None extends Option[Nothing] {
+  override def map[B](f: (Nothing) => B): Option[B] = None
+  override def getOrElse[B >: Nothing](default: => B): B = default
+  override def flatMap[B](f: (Nothing) => Option[B]): Option[B] = None
+  override def orElse[B >: Nothing](ob: => Option[B]): Option[B] = ob
+  override def filter(f: (Nothing) => Boolean): Option[Nothing] = None
+}
 
 object Option {
   def failingFn(i: Int): Int = {
